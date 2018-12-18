26478606c073e5eb3d9d6d31ec22e3bc97ce2568
Muqsith Irfan <mui@one.com>
2018-12-18 19:52:39 +0400

    Fixed tc

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,11 +6,9 @@ const getReadLineStream = require('../index');
 
 describe('ReadLineStream - ', function () {
     it('should read all 60 lines from file', function (done) {
-        //const dataFile = path.resolve(__dirname, 'data', 'sample-data.txt');
-        //const outFile = path.resolve(__dirname, 'data', 'out.txt');
-        this.timeout(30 * 1000); // 5 seconds
-        const dataFile = path.resolve('/home/mui/Development/data/enwiktionary.json');
-        const outFile = path.resolve('/home/mui/Development/data/enwiktionary-out.json');
+        const dataFile = path.resolve(__dirname, 'data', 'sample-data.txt');
+        const outFile = path.resolve(__dirname, 'data', 'out.txt');
+        //this.timeout(30 * 1000); // 5 seconds
         getReadLineStream(dataFile)
         .then((readLineStream) => {
             const writeStream = fs.createWriteStream(outFile);
